4fa967d7e067adbe4f385ebb8806f6e098a7df14
0011557 <0011557@S4H26349.insags.com>
2018-05-24 17:44:26 +0200

    Subida de query

--- IsbanCmis/src/main/java/org/apache/chemistry/opencmis/isbanutil/QueryUtil.java
+++ IsbanCmis/src/main/java/org/apache/chemistry/opencmis/isbanutil/QueryUtil.java
@@ -6,8 +6,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import prodoc.PDObjDefs;
-
 public class QueryUtil {
 
 	private static Map<String, String> listaTradMetadatas;
@@ -36,20 +34,19 @@ public class QueryUtil {
 	}
 
 	private static List<String> getParamFrom(String statement) {
-		statement = statement.toLowerCase();
 		List<String> listaTipos = new ArrayList<>();
 		String[] afrom;
 		if (statement.contains("where")) {
-			afrom = statement.substring(statement.indexOf(" from ")+6, statement.indexOf(" where ")).split(",");
+			afrom = statement.substring(statement.toLowerCase().indexOf(" from ")+6, statement.toLowerCase().indexOf(" where ")).split(",");
 		}else if (statement.contains("order by")){
-			afrom = statement.substring(statement.indexOf(" from ")+6, statement.indexOf(" order by ")).split(",");
+			afrom = statement.substring(statement.toLowerCase().indexOf(" from ")+6, statement.toLowerCase().indexOf(" order by ")).split(",");
 		}else {
-			afrom = statement.substring(statement.indexOf(" from ")+6, statement.length()).split(",");
+			afrom = statement.substring(statement.toLowerCase().indexOf(" from ")+6, statement.toLowerCase().length()).split(",");
 		}
 		String[] atipo = null;
 		for(String tipo : afrom) {
-			if(tipo.contains(" AS ")) {
-				tipo=tipo.substring(0,tipo.indexOf(" AS "));
+			if(tipo.toLowerCase().contains(" as ")) {
+				tipo=tipo.substring(0,tipo.toLowerCase().indexOf(" as "));
 			}
 			if(tipo.contains(":")) {
 				atipo= tipo.split(":");
@@ -65,11 +62,11 @@ public class QueryUtil {
 
 	private static List<String> getParamSelect(String sql) {
 		sql = sql.toLowerCase();
-		String select = sql.substring(6, sql.indexOf("from")).trim();
+		String select = sql.substring(6, sql.toLowerCase().indexOf("from")).trim();
 		String[] aSelect = select.split(",");
 		List<String> salida = new ArrayList<>();
 		for (String a : aSelect) {
-		    a=a.trim();
+			a=a.trim();
 			salida.add((listaTradMetadatas.get(a) != null) ? listaTradMetadatas.get(a) : a);
 
 		}
