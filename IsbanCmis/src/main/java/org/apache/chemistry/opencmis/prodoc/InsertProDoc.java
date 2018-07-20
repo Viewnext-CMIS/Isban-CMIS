8e6a70d19e557d24124b1fd4aa4b9f9e29752a83
0011557 <0011557@S4H26349.insags.com>
2018-07-20 09:02:45 +0200

    Sergio
    Subida del tratamiento de insert
    Subida de test para insert con atributos custom
    Subida de pom con dependencias del cliente Cmis

--- IsbanCmis/src/main/java/org/apache/chemistry/opencmis/prodoc/InsertProDoc.java
+++ IsbanCmis/src/main/java/org/apache/chemistry/opencmis/prodoc/InsertProDoc.java
@@ -10,6 +10,7 @@ import org.apache.chemistry.opencmis.commons.data.ContentStream;
 import org.apache.chemistry.opencmis.commons.data.Properties;
 import org.apache.chemistry.opencmis.commons.data.PropertyData;
 
+import prodoc.Attribute;
 import prodoc.PDDocs;
 import prodoc.PDException;
 import prodoc.PDFolders;
@@ -21,98 +22,114 @@ import prodoc.PDFolders;
  */
 public class InsertProDoc {
 
-    public InsertProDoc() {
+	public InsertProDoc() {
 
-    }
+	}
 
-    /**
-     * Método para crear carpetas
-     * 
-     * @param properties
-     * @param sesion
-     * @param parentId
-     * @return Id de la carpeta creada
-     */
-    public static String crearCarpeta(Properties properties, SesionProDoc sesion, String parentId) { 
-        PDFolders folder = null;
+	/**
+	 * Método para crear carpetas
+	 * 
+	 * @param properties
+	 * @param sesion
+	 * @param parentId
+	 * @return Id de la carpeta creada
+	 */
+	public static String crearCarpeta(Properties properties, SesionProDoc sesion, String parentId) {
+		PDFolders folder = null;
 
-        try {
-            PropertyData<?> nombre = properties.getProperties().get("cmis:name");
+		try {
+			PropertyData<?> nombre = properties.getProperties().get("cmis:name");
 
-            folder = new PDFolders(sesion.getMainSession());
-            folder.setTitle(nombre.getValues().get(0).toString());
-            if (parentId.equals("@root@")) {
-                folder.setParentId("RootFolder");
-            } else {
-                folder.setParentId(parentId);
-            }
-            
-            folder.insert();
+			folder = new PDFolders(sesion.getMainSession());
+			folder.setTitle(nombre.getValues().get(0).toString());
+			if (parentId.equals("@root@")) {
+				folder.setParentId("RootFolder");
+			} else {
+				folder.setParentId(parentId);
+			}
 
-        } catch (PDException e) {
-            e.printStackTrace();
-        }
+			folder.insert();
 
-        return folder.getPDId();
-    }
+		} catch (PDException e) {
+			e.printStackTrace();
+		}
 
-    /**
-     * Método para crear documentos
-     * 
-     * @param properties
-     * @param sesion
-     * @param parentId
-     * @param contentStream
-     * @param typeId 
-     * @return Id del documento creado
-     */
-    public static String crearDocumento(Properties properties, SesionProDoc sesion, String parentId,
-            ContentStream contentStream, String typeId) {
-        PDDocs doc = null;
-        try {
-            PropertyData<?> nombre = properties.getProperties().get("cmis:name");
-            if(typeId.equalsIgnoreCase("cmis:document")) {
-            	typeId="PD_DOCS";
-            }
-           
-            doc = new PDDocs(sesion.getMainSession(), typeId); 
+		return folder.getPDId();
+	}
 
-            doc.setTitle(nombre.getValues().get(0).toString());
-            if (parentId.equals("@root@")) {
-                doc.setParentId("RootFolder");
-            } else {
-            	doc.setParentId(parentId);
-            }
-            doc.setDocDate(new Date());
-            
-            InputStream stream = contentStream.getStream();
-            doc.setStream(stream);
-            
-            String nomAdj=contentStream.getFileName();
-            String ext =nomAdj.substring(nomAdj.indexOf(".")+1).toLowerCase();
-            doc.setMimeType(ext);
-            doc.setName(nomAdj);
-            doc.insert();
+	/**
+	 * Método para crear documentos
+	 * 
+	 * @param properties
+	 * @param sesion
+	 * @param parentId
+	 * @param contentStream
+	 * @param typeId
+	 * @return Id del documento creado
+	 */
+	@SuppressWarnings("unchecked")
+	public static String crearDocumento(Properties properties, SesionProDoc sesion, String parentId,
+			ContentStream contentStream, String typeId) {
+		PDDocs doc = null;
+		try {
+			PropertyData<?> nombre = properties.getProperties().get("cmis:name");
+			if (typeId.equalsIgnoreCase("cmis:document")) {
+				typeId = "PD_DOCS";
+			}
 
-        } catch (PDException e) {
-            e.printStackTrace();
-        }
+			doc = new PDDocs(sesion.getMainSession(), typeId);
 
-        return doc.getPDId();
-    }
+			doc.setTitle(nombre.getValues().get(0).toString());
+			if (parentId.equals("@root@")) {
+				doc.setParentId("RootFolder");
+			} else {
+				doc.setParentId(parentId);
+			}
+			doc.setDocDate(new Date());
 
-    public static String getStreamContents(InputStream in) throws IOException {
+			InputStream stream = contentStream.getStream();
+			doc.setStream(stream);
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-        StringBuilder out = new StringBuilder();
-        String line;
-        while ((line = reader.readLine()) != null) {
-            out.append(line);
-        }
-        System.out.println(out.toString()); // Prints the string content read from input stream
-        reader.close();
+			String nomAdj = contentStream.getFileName();
+			String ext = nomAdj.substring(nomAdj.indexOf(".") + 1).toLowerCase();
+			doc.setMimeType(ext);
+			doc.setName(nomAdj);
+			
+			//Propiedades no estaticas si coinciden
+			for (String propkey : properties.getProperties().keySet()) {
+				PropertyData<?> prop = properties.getProperties().get(propkey);
+				Attribute atrib = doc.getRecord().getAttr(propkey);
+				if (atrib != null) {
+					if (atrib.isMultivalued()) {
+						if (!prop.getValues().isEmpty())
+							atrib.getValuesList().addAll(prop.getValues());
+					} else {
+						atrib.setValue(prop.getFirstValue());
+					}
+				}
+			}
 
-        return out.toString();
-    }
+			doc.insert();
+
+		} catch (PDException e) {
+			e.printStackTrace();
+		}
+
+		return doc.getPDId();
+	}
+
+	public static String getStreamContents(InputStream in) throws IOException {
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		StringBuilder out = new StringBuilder();
+		String line;
+		while ((line = reader.readLine()) != null) {
+			out.append(line);
+		}
+		System.out.println(out.toString()); // Prints the string content read from input stream
+		reader.close();
+
+		return out.toString();
+	}
 
 }
